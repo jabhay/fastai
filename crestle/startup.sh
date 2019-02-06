eafad8118a413a7119875610fbbcbd97bd79f243
Joseph Abhayaratna <Joseph.Abhayaratna@psma.com.au>
2019-02-06 21:40:14 +1100

    Added comments and made training data only download if not there
    Added comments to make the the code more understandable. Also
    altered the training data download to be conditional upon whether
    the data has already been downloaded. The goal here is to avoid
    overwriting any changes to the training dataset that the user has
    made. It forces the user to delete the training data folder
    explicitly if they want to refresh from Github to avoid accidental
    loss of work.

--- crestle/startup.sh
+++ crestle/startup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-mkdir training_data
-curl -o training_data/satimgs.tgz https://raw.githubusercontent.com/jabhay/fastai/master/training_data/satimgs.tgz
-tar xvzf training_data/satimgs.tgz -C training_data
+# Obtain training data if it doesn't already exist
+if [ ! -d "training_data" ]; then
+    mkdir training_data
+    curl -o training_data/satimgs.tgz https://raw.githubusercontent.com/jabhay/fastai/master/training_data/satimgs.tgz
+    tar xvzf training_data/satimgs.tgz -C training_data
+else
+    echo "Training data already exists... if you want to restart from Github, delete the folder and re-run"
+fi
+
+# Get the latest version of the course work to the latest
 cd courses/fast-ai/course-v3/
 git pull
+
+# Update Anaconda to the latest
 conda update conda
+
+# Update the Fast AI library to the latest
 conda install -c fastai fastai
