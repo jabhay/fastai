686d5b423483e022f5443af0cfc32c66569773b0
Joseph Abhayaratna <Joseph.Abhayaratna@psma.com.au>
2019-02-06 21:23:04 +1100

    Added download of training data to startup script

--- crestle/startup.sh
+++ crestle/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir training_data
+curl -o training_data/satimgs.tgz https://raw.githubusercontent.com/jabhay/fastai/master/training_data/satimgs.tgz
+tar xvzf training_data/satimgs.tgz -C training_data
 cd courses/fast-ai/course-v3/
 git pull
 conda update conda
